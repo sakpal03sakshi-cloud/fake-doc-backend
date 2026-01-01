2a9549d1e6d5eea10c582a8cfc74ceff7b50d0af
sakpal03sakshi-cloud <sakpal03sakshi@gmail.com>
2026-01-01 19:02:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+
+from PIL import Image
+import os
+import cv2
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def register():
+    return render_template('register.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    return render_template('upload.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    file = request.files['document']
+    path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(path)
+
+    # OCR
+    img = Image.open(path)
+    text = pytesseract.image_to_string(img)
+
+    # Advanced document validation logic
+    keywords = ['government', 'india', 'name', 'date', 'id']
+    score = sum(1 for k in keywords if k.lower() in text.lower())
+
+    if score >= 3:
+        status = "REAL DOCUMENT"
+        confidence = 90
+        remark = "Document structure & content look valid."
+    else:
+        status = "FAKE DOCUMENT"
+        confidence = 55
+        remark = "Missing official patterns or text."
+
+    return render_template(
+        'result.html',
+        status=status,
+        confidence=confidence,
+        remark=remark
+    )
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=5000, debug=True)
